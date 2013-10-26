329ad280c34a52240bd8f27cc015d01ae0e94787
Avinash Gosavi <aag1091@gmail.com>
2013-10-26 10:13:12 +0530

    initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RefactoringDemo::Application.config.secret_key_base = '365f5a7f3b2f03c51d70c81b9b12a9f7261021cf56d65dd42b1e49057583c691cb0647268b5edb746c64f3c9b1c65b58b881a57981d40580847f2f729a5b8145'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RefactoringDemo::Application.config.session_store :cookie_store, key: '_refactoring-demo_session'
